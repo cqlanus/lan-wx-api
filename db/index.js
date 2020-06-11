32e7971a8e82bbccf442c7ceb2724a5d9839c54c
Chris Lanus <cqlanus@gmail.com>
2020-06-11 16:54:03 -0500

    setting db password on connect

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, null, {
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: 'postgres',
   dialect: 'postgres'
 });
