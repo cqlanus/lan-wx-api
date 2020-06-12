16490555860edf2d41e99011c7c77375fea25ebf
Chris Lanus <cqlanus@gmail.com>
2020-06-11 21:44:11 -0500

    logging in seed script

--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
+const { POSTGRES_DB, POSTGRES_USER } = process.env
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: 'postgres',
   dialect: 'postgres'
 });
 
+console.log({ POSTGRES_DB, POSTGRES_USER })
+console.log({ sequelize })
 module.exports = sequelize
